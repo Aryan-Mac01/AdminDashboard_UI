e70cf56a64d40ae52ba1d07e84a8f9d00b71e1a3
Aryan Mishra <29651csiot@gmail.com>
2026-01-12 12:02:01 +0530

    initial commit

---
+++ AdminDashboard_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminDashboard_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
